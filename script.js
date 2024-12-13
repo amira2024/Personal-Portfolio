7c874a1beeb904ff1b1be056f151fdaddcbe55c1
amira2024 <amiragarba13@gmail.com>
2024-12-13 15:14:57 -0500

    Visual formatting , changd colors added interactive background and fixed more alignment issues

--- script.js
+++ script.js
@@ -77,3 +77,106 @@ document.getElementById('contact-form').addEventListener('submit', function(even
         event.preventDefault();
     }
 });
+
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 100, // Number of particles
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: "#bb0b0b" // Particle color
+        },
+        shape: {
+            type: "circle", // Particle shape
+            stroke: {
+                width: 2,
+                color: "#bb0b0b" // Border color
+            }
+        },
+        opacity: {
+            value: 0.7,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false
+            }
+        },
+        size: {
+            value: 4,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 20,
+                size_min: 0.1,
+                sync: false
+            }
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#bb0b0b",
+            opacity: 0.6,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 3,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+            }
+        }
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: true,
+                mode: "repulse"
+            },
+            onclick: {
+                enable: true,
+                mode: "push"
+            },
+            resize: true
+        },
+        modes: {
+            grab: {
+                distance: 140,
+                line_linked: {
+                    opacity: 1
+                }
+            },
+            bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 8,
+                speed: 3
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4
+            },
+            push: {
+                particles_nb: 4
+            },
+            remove: {
+                particles_nb: 2
+            }
+        }
+    },
+    retina_detect: true
+});
